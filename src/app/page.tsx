c42f16b4e8b6d292a447cae433b371659edad468
midCenturyDude7 <mgriffes@gmail.com>
2023-10-05 21:24:13 -0700

    Minor edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">LandingPage</div>
+    <div className="flex justify-center items-center h-screen">Home Page</div>
   );
 }
