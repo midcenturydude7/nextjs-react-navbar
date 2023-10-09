ae0a3563b4d2f6117a4e549522e5a12d72461495
midCenturyDude7 <mgriffes@gmail.com>
2023-10-08 20:14:27 -0700

    Completed animation of navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -45,8 +45,28 @@ export default function Navbar() {
               className={`px-4 py-2 rounded-md text-sm lg:text-base relative no-underline duration-300 ease-in ${
                 isActive ? "text-zinc-100" : "text-zinc-400"
               }`}
-              href={item.path}>
+              href={item.path}
+              data-active={isActive}
+              onMouseOver={() => setHoveredPath(item.path)}
+              onMouseLeave={() => setHoveredPath(pathname)}>
               <span>{item.name}</span>
+              {item.path === hoveredPath && (
+                <motion.div
+                  className="absolute bottom-0 left-0 h-full bg-stone-800/80 rounded-md -z-10"
+                  layoutId="navbar"
+                  aria-hidden="true"
+                  style={{
+                    width: "100%",
+                  }}
+                  transition={{
+                    type: "spring",
+                    bounce: 0.25,
+                    stiffness: 130,
+                    damping: 9,
+                    duration: 0.3,
+                  }}
+                />
+              )}
             </Link>
           );
         })}
